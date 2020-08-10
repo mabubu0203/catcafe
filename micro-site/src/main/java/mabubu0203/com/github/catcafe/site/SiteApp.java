7e033efca5f715d61cced9a6e79fa9e653ce1c77
mabubu0203 <mabu_ip@msn.com>
2020-08-10 22:39:40 +0900

    :pencil: Fix (#23)
    vueの読み込み追加
    子プロジェクト追加
    nodeの読み込み追加
    ignoreファイルにgradle周りを追加

---
+++ micro-site/src/main/java/mabubu0203/com/github/catcafe/site/SiteApp.java
@@ -0,0 +1,13 @@
+package mabubu0203.com.github.catcafe.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteApp.class, args);
+    }
+
+}
