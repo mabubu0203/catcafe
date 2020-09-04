c5b4073678cc04fc38b2b01f27a095f2c4cc75b8
uratamanabu <mabu_ip@msn.com>
2020-09-04 14:19:00 +0900

    swaggerアノテーションをキャストAPIに追加

--- micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/cast/CastApiController.java
+++ micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/cast/CastApiController.java
@@ -1,5 +1,9 @@
 package mabubu0203.com.github.catcafe.api.controller.cast;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.api.CastApi;
 import org.openapitools.model.*;
@@ -15,26 +19,80 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class CastApiController implements CastApi {
 
+    @Operation(
+            tags = {"cast",},
+            summary = "キャスト登録API",
+            description = "キャストを1件登録する",
+            operationId = "castCreate",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = PostObject.class))),
+                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
+            }
+    )
     @Override
     public CompletableFuture<ResponseEntity<PostObject>> castCreate(String cats, Integer storeId, @Valid CastCreate castCreate) {
         return null;
     }
 
+    @Operation(
+            tags = {"cast",},
+            summary = "キャスト削除API",
+            description = "キャストを1件論理削除する",
+            operationId = "castDelete",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "正常系"),
+                    @ApiResponse(responseCode = "404", description = "Idが見つからない"),
+                    @ApiResponse(responseCode = "409", description = "排他失敗"),
+            }
+    )
     @Override
     public CompletableFuture<ResponseEntity<Void>> castDelete(String cats, Integer storeId, Integer castId, @NotNull @Valid Integer version) {
         return null;
     }
 
+    @Operation(
+            tags = {"cast",},
+            summary = "キャスト詳細取得API",
+            description = "キャスト詳細を1件取得する",
+            operationId = "castFind",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = CastDetail.class))),
+                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
+                    @ApiResponse(responseCode = "404", description = "Idが見つからない"),
+            }
+    )
     @Override
     public CompletableFuture<ResponseEntity<CastDetail>> castFind(String cats, Integer storeId, Integer castId) {
         return null;
     }
 
+    @Operation(
+            tags = {"cast",},
+            summary = "キャスト一覧取得API",
+            description = "キャストを取得する",
+            operationId = "castSearch",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "正常系"),
+                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
+            }
+    )
     @Override
     public CompletableFuture<ResponseEntity<List<CastDetail>>> castSearch(String cats, @Valid List<Integer> storeIds) {
         return null;
     }
 
+    @Operation(
+            tags = {"cast",},
+            summary = "キャスト更新API",
+            description = "キャストを1件更新する",
+            operationId = "castUpdate",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = PatchObject.class))),
+                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
+                    @ApiResponse(responseCode = "404", description = "Idが見つからない"),
+                    @ApiResponse(responseCode = "409", description = "排他失敗"),
+            }
+    )
     @Override
     public CompletableFuture<ResponseEntity<PatchObject>> castUpdate(String cats, Integer storeId, Integer castId, @Valid CastUpdate castUpdate) {
         return null;
