54e9546743cc37947f3d8cc2f07f055b756d7bec
uratamanabu <mabu_ip@msn.com>
2020-09-08 16:23:11 +0900

    FAQのDDLなどを追加

---
+++ micro-infra/src/main/java/mabubu0203/com/github/catcafe/infra/source/jpa/FrequentlyAskedQuestionSource.java
@@ -0,0 +1,9 @@
+package mabubu0203.com.github.catcafe.infra.source.jpa;
+
+import mabubu0203.com.github.catcafe.common.source.jpa.TableSource;
+import mabubu0203.com.github.catcafe.infra.source.jpa.entity.table.FrequentlyAskedQuestion;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FrequentlyAskedQuestionSource extends TableSource<FrequentlyAskedQuestion, Integer> {
+}
---
+++ micro-infra/src/main/java/mabubu0203/com/github/catcafe/infra/source/jpa/entity/table/FrequentlyAskedQuestion.java
@@ -0,0 +1,43 @@
+package mabubu0203.com.github.catcafe.infra.source.jpa.entity.table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import mabubu0203.com.github.catcafe.common.source.jpa.entity.BaseTable;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Accessors(chain = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name = "frequently_asked_question")
+@Where(clause = "deleted_flag = false")
+public class FrequentlyAskedQuestion extends BaseTable {
+
+    @Id
+    @TableGenerator(
+            name = "SeqGenerator",
+            table = "sequence_generator",
+            pkColumnName = "name",
+            pkColumnValue = "frequently_asked_question.id",
+            valueColumnName = "value"
+    )
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "SeqGenerator")
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "store_id", nullable = false)
+    private Integer storeId;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "question_summary\t", nullable = false)
+    private String questionSummary;
+
+    @Column(name = "answer_summary", nullable = false)
+    private String answerSummary;
+
+}
