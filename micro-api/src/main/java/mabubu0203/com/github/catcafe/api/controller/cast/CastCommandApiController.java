c085a95ec0ba96161e5de77e8710e2785dbf1f52
uratamanabu <mabu_ip@msn.com>
2020-10-17 21:00:17 +0900

    リファクタリング

--- micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/cast/CastCommandApiController.java
+++ micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/cast/CastCommandApiController.java
@@ -2,47 +2,36 @@ package mabubu0203.com.github.catcafe.api.controller.cast;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import mabubu0203.com.github.catcafe.api.controller.cast.helper.request.CastCatCreateRequestMapper;
 import mabubu0203.com.github.catcafe.api.controller.cast.helper.request.CastCreateRequestMapper;
-import mabubu0203.com.github.catcafe.api.controller.cast.helper.request.CastFindRequestMapper;
-import mabubu0203.com.github.catcafe.api.controller.cast.helper.request.CastSearchRequestMapper;
 import mabubu0203.com.github.catcafe.api.controller.cast.helper.response.CastCatCreateResponseMapper;
 import mabubu0203.com.github.catcafe.api.controller.cast.helper.response.CastCreateResponseMapper;
-import mabubu0203.com.github.catcafe.api.controller.cast.helper.response.CastFindResponseMapper;
-import mabubu0203.com.github.catcafe.api.controller.cast.helper.response.CastSearchResponseMapper;
 import mabubu0203.com.github.catcafe.api.controller.cast.service.CastCatResisterService;
 import mabubu0203.com.github.catcafe.api.controller.cast.service.CastRegisterService;
-import mabubu0203.com.github.catcafe.api.controller.cast.service.CastSearchService;
-import org.openapitools.api.CastApi;
+import org.openapitools.api.CastCommandApi;
 import org.openapitools.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class CastApiController implements CastApi {
+public class CastCommandApiController implements CastCommandApi {
 
     private final CastCatResisterService castCatResisterService;
     private final CastRegisterService castResisterService;
-    private final CastSearchService castSearchService;
 
     @Operation(
-            tags = {"cast",},
+            tags = {"cast_command",},
             summary = "キャスト(猫)登録API",
             description = "キャスト(猫)を1件登録する",
             operationId = "castCatCreate",
@@ -65,7 +54,7 @@ public class CastApiController implements CastApi {
     }
 
     @Operation(
-            tags = {"cast",},
+            tags = {"cast_command",},
             summary = "キャスト(猫)削除API",
             description = "キャスト(猫)を1件論理削除する",
             operationId = "castCatDelete",
@@ -85,44 +74,7 @@ public class CastApiController implements CastApi {
     }
 
     @Operation(
-            tags = {"cast",},
-            summary = "キャスト(猫)詳細取得API",
-            description = "キャスト(猫)詳細を1件取得する",
-            operationId = "castCatFind",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = CastCatFindResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
-                    @ApiResponse(responseCode = "404", description = "Idが見つからない"),
-            }
-    )
-    @Override
-    public Mono<ResponseEntity<CastCatFindResponse>> castCatFind(
-            @Parameter(description = "カフェ識別子", schema = @Schema(allowableValues = {"cats"})) String cats,
-            Integer castCatId,
-            ServerWebExchange exchange) {
-        return null;
-    }
-
-    @Operation(
-            tags = {"cast",},
-            summary = "キャスト(猫)一覧取得API",
-            description = "キャスト(猫)を取得する",
-            operationId = "castCatSearch",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = CastCatSearchResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
-            }
-    )
-    @Override
-    public Mono<ResponseEntity<CastCatSearchResponse>> castCatSearch(
-            @Parameter(description = "カフェ識別子", schema = @Schema(allowableValues = {"cats"})) String cats,
-            @Valid Integer size,
-            ServerWebExchange exchange) {
-        return null;
-    }
-
-    @Operation(
-            tags = {"cast",},
+            tags = {"cast_command",},
             summary = "キャスト(猫)更新API",
             description = "キャスト(猫)を1件更新する",
             operationId = "castCatUpdate",
@@ -143,7 +95,7 @@ public class CastApiController implements CastApi {
     }
 
     @Operation(
-            tags = {"cast",},
+            tags = {"cast_command",},
             summary = "キャスト登録API",
             description = "キャストを1件登録する",
             operationId = "castCreate",
@@ -167,7 +119,7 @@ public class CastApiController implements CastApi {
     }
 
     @Operation(
-            tags = {"cast",},
+            tags = {"cast_command",},
             summary = "キャスト削除API",
             description = "キャストを1件論理削除する",
             operationId = "castDelete",
@@ -188,63 +140,7 @@ public class CastApiController implements CastApi {
     }
 
     @Operation(
-            tags = {"cast",},
-            summary = "キャスト詳細取得API",
-            description = "キャスト詳細を1件取得する",
-            operationId = "castFind",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = CastFindResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
-                    @ApiResponse(responseCode = "404", description = "Idが見つからない"),
-            }
-    )
-    @CrossOrigin
-    @Override
-    public Mono<ResponseEntity<CastFindResponse>> castFind(
-            @Parameter(description = "カフェ識別子", schema = @Schema(allowableValues = {"cats"})) String cats,
-            @Parameter(description = "店舗ID", schema = @Schema(type = "integer")) Integer storeId,
-            @Parameter(description = "キャストID", schema = @Schema(type = "integer")) Integer castId,
-            ServerWebExchange exchange) {
-        return new CastFindRequestMapper(
-                cats, storeId, castId).get()
-                .map(this.castSearchService::promise)
-                .flatMap(Mono::fromCompletionStage)
-                .map(new CastFindResponseMapper())
-                .map(ResponseEntity.status(HttpStatus.OK)::body);
-    }
-
-
-    @Operation(
-            tags = {"cast",},
-            summary = "キャスト一覧取得API",
-            description = "キャストを取得する",
-            operationId = "castSearch",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = CastSearchResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
-            }
-    )
-    @CrossOrigin
-    @Override
-    public Mono<ResponseEntity<CastSearchResponse>> castSearch(
-            @Parameter(description = "カフェ識別子", schema = @Schema(allowableValues = {"cats"})) String cats,
-            @Parameter(description = "店舗ID") @Valid List<Integer> storeIds,
-            @Parameter(description = "キャストID") @Valid List<Integer> castIds,
-            @Parameter(description = "取得ページ", schema = @Schema(type = "integer", maxProperties = 100)) @Valid @Min(0) @Max(100) Integer page,
-            @Parameter(description = "取得サイズ", schema = @Schema(type = "integer", minProperties = 1, maxProperties = 20)) @Valid @Min(1) @Max(20) Integer size,
-            @Parameter(description = "ソートキー", array = @ArraySchema(schema = @Schema(allowableValues = {"store_id.asc", "store_id.desc"}))) @Valid List<String> sortKeys,
-            ServerWebExchange exchange) {
-        return new CastSearchRequestMapper(
-                cats, storeIds, castIds,
-                page, size, sortKeys).get()
-                .map(this.castSearchService::promise)
-                .flatMap(Mono::fromCompletionStage)
-                .map(new CastSearchResponseMapper())
-                .map(ResponseEntity.status(HttpStatus.OK)::body);
-    }
-
-    @Operation(
-            tags = {"cast",},
+            tags = {"cast_command",},
             summary = "キャスト更新API",
             description = "キャストを1件更新する",
             operationId = "castUpdate",
--- micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/provide/service/ProvideServiceCommandApiController.java
+++ micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/provide/service/ProvideServiceCommandApiController.java
@@ -6,30 +6,22 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import mabubu0203.com.github.catcafe.api.controller.provide.service.helper.request.ProvideServiceSearchRequestMapper;
-import mabubu0203.com.github.catcafe.api.controller.provide.service.helper.response.ProvideServiceSearchResponseMapper;
-import mabubu0203.com.github.catcafe.api.controller.provide.service.service.ProvideServiceSearchService;
-import org.openapitools.api.ProvideServiceApi;
+import org.openapitools.api.ProvideServiceCommandApi;
 import org.openapitools.model.*;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class ProvideServiceApiController implements ProvideServiceApi {
-
-    private final ProvideServiceSearchService searchService;
+public class ProvideServiceCommandApiController implements ProvideServiceCommandApi {
 
     @Operation(
-            tags = {"provide_service",},
+            tags = {"provide_service_command",},
             summary = "提供サービス登録API",
             description = "提供サービスを1件登録する",
             operationId = "provideServiceCreate",
@@ -48,7 +40,7 @@ public class ProvideServiceApiController implements ProvideServiceApi {
     }
 
     @Operation(
-            tags = {"provide_service",},
+            tags = {"provide_service_command",},
             summary = "提供サービス削除API",
             description = "提供サービスを1件論理削除する",
             operationId = "provideServiceDelete",
@@ -69,51 +61,7 @@ public class ProvideServiceApiController implements ProvideServiceApi {
     }
 
     @Operation(
-            tags = {"provide_service",},
-            summary = "提供サービス詳細取得API",
-            description = "提供サービス詳細を1件取得する",
-            operationId = "provideServiceFind",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = ProvideServiceFindResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
-                    @ApiResponse(responseCode = "404", description = "Idが見つからない"),
-            }
-    )
-    @Override
-    public Mono<ResponseEntity<ProvideServiceFindResponse>> provideServiceFind(
-            @Parameter(description = "カフェ識別子", schema = @Schema(allowableValues = {"cats"})) String cats,
-            Integer storeId,
-            Integer provideServiceId,
-            ServerWebExchange exchange) {
-        return null;
-    }
-
-    @Operation(
-            tags = {"provide_service",},
-            summary = "提供サービス一覧取得API",
-            description = "提供サービスを取得する",
-            operationId = "provideServiceSearch",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "正常系", content = @Content(schema = @Schema(implementation = ProvideServiceSearchResponse.class))),
-                    @ApiResponse(responseCode = "400", description = "バリデーションエラー", content = @Content(schema = @Schema(implementation = ValidationResult.class))),
-            }
-    )
-    @CrossOrigin
-    @Override
-    public Mono<ResponseEntity<ProvideServiceSearchResponse>> provideServiceSearch(
-            @Parameter(description = "カフェ識別子", schema = @Schema(allowableValues = {"cats"})) String cats,
-            @Parameter(description = "店舗ID") @Valid List<Integer> storeIds,
-            ServerWebExchange exchange) {
-        return new ProvideServiceSearchRequestMapper(
-                cats, storeIds).get()
-                .map(this.searchService::promise)
-                .flatMap(Mono::fromCompletionStage)
-                .map(new ProvideServiceSearchResponseMapper())
-                .map(ResponseEntity.status(HttpStatus.OK)::body);
-    }
-
-    @Operation(
-            tags = {"provide_service",},
+            tags = {"provide_service_command",},
             summary = "提供サービス更新API",
             description = "提供サービスを1件更新する",
             operationId = "provideServiceUpdate",
