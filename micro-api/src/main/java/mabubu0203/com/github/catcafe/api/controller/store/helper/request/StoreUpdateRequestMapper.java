bcfdecfc95c0eed2c365d31790713d21d7af7841
uratamanabu <mabu_ip@msn.com>
2021-11-30 00:33:22 +0900

    #211
    店舗更新API実装

--- micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/store/helper/request/StoreUpdateRequestMapper.java
+++ micro-api/src/main/java/mabubu0203/com/github/catcafe/api/controller/store/helper/request/StoreUpdateRequestMapper.java
@@ -1,5 +1,7 @@
 package mabubu0203.com.github.catcafe.api.controller.store.helper.request;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import mabubu0203.com.github.catcafe.api.controller.store.service.model.input.StoreModifyServiceInput;
 import mabubu0203.com.github.catcafe.common.controller.mapper.request.UpdateRequestMapper;
@@ -13,12 +15,41 @@ public class StoreUpdateRequestMapper implements
   private final Integer storeId;
 
   @Override
-  public StoreModifyServiceInput apply(StoreUpdateRequest storeUpdate) {
+  public StoreModifyServiceInput apply(StoreUpdateRequest request) {
+    var contact = request.getContact();
+    var contactObject = StoreModifyServiceInput.ContactObject.builder()
+        .phoneNumber(contact.getPhoneNumber())
+        .mailAddress(contact.getMailAddress())
+        .build();
+    var address = request.getAddress();
+    var addressObject = StoreModifyServiceInput.AddressObject.builder()
+        .postalCode(address.getPostalCode())
+        .prefectureCode(address.getPrefectureCode())
+        .address1(address.getAddress1())
+        .address2(address.getAddress2())
+        .address3(address.getAddress3())
+        .streetAddress(address.getStreetAddress())
+        .buildingName(address.getBuildingName())
+        .supplement(address.getSupplement())
+        .build();
+    var hours = request.getHours();
+    var hoursObject = StoreModifyServiceInput.HoursObject.builder()
+        .openingTime(LocalTime.parse(hours.getOpeningTime(), DateTimeFormatter.ofPattern("HH:mm")))
+        .closingTime(LocalTime.parse(hours.getClosingTime(), DateTimeFormatter.ofPattern("HH:mm")))
+        .supplement(hours.getSupplement())
+        .build();
+
     return StoreModifyServiceInput.builder()
         .cats(this.cats)
         .storeId(this.storeId)
-        .name(storeUpdate.getName())
-        .version(storeUpdate.getVersion())
+        .name(request.getName())
+        .contact(contactObject)
+        .address(addressObject)
+        .openDate(request.getOpenDate())
+        .closeDate(request.getCloseDate())
+        .hours(hoursObject)
+        .memo(request.getMemo())
+        .version(request.getVersion())
         .build();
   }
 
